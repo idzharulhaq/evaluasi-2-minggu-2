092e2aba4b0f833d31238bc91b7fffd49f870a13
idzharulhaq <ihaq1013@gmail.com>
2022-08-13 10:45:32 +0700

    evalausi 2 minggu

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// soal 1
+
+let myarray = ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd']
+
+let item = 9
+for (let item of myarray) {
+    console.log(item)
+}
+
+// soal 2
+let keuntungan = {
+    senin: 55400,
+    selasa: 61600,
+    rabu: 49300,
+    kamis: 53200,
+    jumat: 54500,
+    sabtu: 51500,
+    minggu: 70000
+}
+let hasil = 7
+for (let hasil in keuntungan) {
+    console.log(hasil)
+}
+
+// soal 3
+
+let a =[["12:10","tidak dikenal"],["12:14","tidak dikenal"]]
+
+console.table(a)
+
+// #pesantrenitbisagakgakbisaharusbisa
